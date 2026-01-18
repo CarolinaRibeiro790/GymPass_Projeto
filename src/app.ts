fe5f19802d5e1e73cf406ffcda839932eba1a239
CarolinaRibeiro790 <carolribeirog20@gmail.com>
2026-01-18 20:15:34 -0300

    Criando banco, tabelas e relacionamentos

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from "fastify";
 
+import { PrismaClient } from "generated/prisma/client.js";
+
 export const app = fastify();
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    nome: 'Carol Ribeiro',
+    email: 'carol@gamil.com'
+  }
+})
